5d5b318a14a0550bdc7329a43fd9cd59ab76f39b
Marcosnachi <mnachimowicz@udesa.edu.ar>
2021-09-13 09:05:55 -0300

    Ver mas

--- proyecto-react/src/components/Card/Card.js
+++ proyecto-react/src/components/Card/Card.js
@@ -1,52 +1,52 @@
-import React, {Component} from 'react';
-import './card.css';
+// import React, {Component} from 'react';
+// import './card.css';
 
-class Card extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            viewMore: false,
-            text:'Ver más',
-            selected: false,
-        }
-    }
-    viewMore(){
-        if(this.state.viewMore){
-            this.setState({
-                viewMore: false,
-                text: 'Ver más'
-            })
-        } else {
-            this.setState({
-                viewMore: true,
-                text: 'ver menos'
-            })            
-        }
-    }
+// class Card extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             viewMore: false,
+//             text:'Ver más',
+//             selected: false,
+//         }
+//     }
+//     viewMore(){
+//         if(this.state.viewMore){
+//             this.setState({
+//                 viewMore: false,
+//                 text: 'Ver más'
+//             })
+//         } else {
+//             this.setState({
+//                 viewMore: true,
+//                 text: 'ver menos'
+//             })            
+//         }
+//     }
 
-    selected(){
-        if(this.state.selected){
-            this.setState({
-                selected: false,
-            })
-        } else {
-            this.setState({
-                selected: true,
-            })
-        }
-    }
+//     selected(){
+//         if(this.state.selected){
+//             this.setState({
+//                 selected: false,
+//             })
+//         } else {
+//             this.setState({
+//                 selected: true,
+//             })
+//         }
+//     }
 
-    render(){
-        return (
-            <div className={`character-card ${this.state.selected ? 'active' : ''}`} onDoubleClick={ ()=>this.selected()}>
-                <img src={`/assets/images/characters/${this.props.data.img}`} alt="" />
-                <h4>{this.props.data.name}</h4>
-                <p>{this.props.data.description}</p>
-                <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Universe: {this.props.data.extra}</p>
-                <p className='more' onClick={()=>this.viewMore()}>{this.state.text}</p>
-            </div>
-        );
-    }
-}
+//     render(){
+//         return (
+//             <div className={`character-card ${this.state.selected ? 'active' : ''}`} onDoubleClick={ ()=>this.selected()}>
+//                 <img src={`/assets/images/characters/${this.props.data.img}`} alt="" />
+//                 <h4>{this.props.data.name}</h4>
+//                 <p>{this.props.data.description}</p>
+//                 <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Universe: {this.props.data.extra}</p>
+//                 <p className='more' onClick={()=>this.viewMore()}>{this.state.text}</p>
+//             </div>
+//         );
+//     }
+// }
 
-export default Card
+// export default Card
--- proyecto-react/src/components/Movie/Movie.js
+++ proyecto-react/src/components/Movie/Movie.js
@@ -5,14 +5,33 @@ import "./styles.css";
 export default class Movie extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      clase: 'hide',
+      mensaje: 'ver más'
+    }
   }
+
+  handleShow(){
+    if (this.state.clase === 'hide'){
+        this.setState({
+            clase: 'show',
+            mensaje: "ver menos"
+        })
+        } else {
+        this.setState({
+            clase: 'hide',
+            mensaje: "ver mas"
+        })   
+    }
+}
 
   render() {
     return (
       <div className="movie">
         <img src={ 'https://image.tmdb.org/t/p/w342' + this.props.poster_path} alt="" />
         <h4 className= "titulo"> {this.props.title} </h4>
-        <h4 className= "descripcion"> {this.props.overview}</h4>
+        <p className= "more" onClick = {() => this.handleShow()}>{this.state.mensaje}</p>
+        <h4 className= {this.state.clase}> {this.props.overview}</h4>
         {/* <button onClick={() => this.props.removerPersonaje(this.props.name)}>
           {" "}
           Eliminar personaje
